fad894313fae940f138679f63c670020ffdbe863
lukaskreibig <lukatze@posteo.de>
2021-07-08 15:06:38 +0200

    join table leaderboard and weight delta

--- models/leaderboard-models.js
+++ models/leaderboard-models.js
@@ -3,9 +3,12 @@ const connection = require("../db/config");
 const Leaderboard = {};
 
 Leaderboard.findUsers = (callback) => {
-  connection.query("SELECT * FROM beekeeper", (err, results, fields) => {
-    callback(err, results, fields);
-  });
+  connection.query(
+    "SELECT beekeeper.beekeeper_id, beekeeper.username, weight_delta.weight_delta, beekeeper.email, beekeeper.city, beekeeper.country, beekeeper.country, beekeeper.experience, beekeeper.beehives, beekeeper.apiaries, beekeeper.profile_picture FROM beekeeper INNER JOIN weight_delta ON weight_delta.id_weight_delta=beekeeper.beekeeper_id;",
+    (err, results, fields) => {
+      callback(err, results, fields);
+    }
+  );
 };
 
 module.exports = Users;
