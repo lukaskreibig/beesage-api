12775b36f8be582e550df890237c2829c6352d65
PamelaFeijo <pamelaffeijo@gmail.com>
2021-07-16 14:52:04 +0200

    Add .env for key

--- controllers/login-controller.js
+++ controllers/login-controller.js
@@ -1,8 +1,9 @@
 const bcrypt = require("bcrypt");
 const Users = require("../models/user-models");
 const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
-const userSignin = (req, res) => {
+const userSignin = (req, res, next) => {
     Users.getUser(req.body.username, (err, results) => {       
         if (err) {
             res.status(500).send('Server Error, we could not find that user.')
@@ -10,9 +11,10 @@ const userSignin = (req, res) => {
            bcrypt.compare(req.body.password, results[0].password)
             .then(isThereAMatch => {
               if(isThereAMatch){
-                const token = jwt.sign({ id: results[0].beekeeper_id }, "your-secret-key")                
+                const token = jwt.sign({ id: results[0].beekeeper_id }, process.env.SECRET_KEY)                
                 res.status(200).cookie('token', token, { httpOnly: true })
                 req.userId = results[0].beekeeper_id
+                res.json(results)
                 next()
                 // console.log(results[0].beekeeper_id)
               }else{
