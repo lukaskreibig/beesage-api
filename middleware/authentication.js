f4e9a5f081859b15d49630a8dd7f248625358626
PamelaFeijo <pamelaffeijo@gmail.com>
2021-07-19 11:55:33 +0200

    test

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -4,14 +4,13 @@ const authentication = (req, res, next) => {
   console.log(req.cookies.token)
   if (!req.cookies.token) {
     
-    res.send("Something went wrong.")   
+    res.send(`Something went wrong.${req.cookies.token}`)   
   } else {
     jwt.verify(req.cookies.token, process.env.SECRET_KEY, (err, decoded) => {
       if(err) {
         res.send("Wrong Access.")
       }
-      req.beekeeper_id = decoded.id
-      req.test = req.cookies.token
+      req.beekeeper_id = decoded.id     
       next();
     })
   }  
