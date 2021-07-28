e77023f4bd4d7b09798a3394ee6c79c85b53576a
lukaskreibig <lukatze@posteo.de>
2021-07-28 18:38:37 +0200

    add connection release

--- controllers/leaderboard-controller.js
+++ controllers/leaderboard-controller.js
@@ -15,7 +15,7 @@ const findWdByDays = (req, res) => {
   Leaderboard.findWdDays(req.params.day)
     .then((results) => {
       if (results) {
-        res.json(results).connection.release();
+        connection.release().res.json(results);
       } else res.status(404).send("Not found");
     })
     .catch((err) => {
