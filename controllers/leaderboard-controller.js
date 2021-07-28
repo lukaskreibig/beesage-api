452fe87a8aed69ab516588797b0813c6af2c56a6
lukaskreibig <lukatze@posteo.de>
2021-07-28 18:35:42 +0200

    add connection release

--- controllers/leaderboard-controller.js
+++ controllers/leaderboard-controller.js
@@ -14,14 +14,14 @@ const getAllWd = (req, res) => {
 const findWdByDays = (req, res) => {
   Leaderboard.findWdDays(req.params.day)
     .then((results) => {
+      connection.release();
       if (results) res.json(results);
       else res.status(404).send("Not found");
-      connection.release();
     })
     .catch((err) => {
       console.log(err);
-      res.status(500).send(`Error retrieving from databases ${err}`);
       connection.release();
+      res.status(500).send(`Error retrieving from databases ${err}`);
     });
 };
 
