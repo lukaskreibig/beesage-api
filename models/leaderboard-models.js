96044abc825463b076fea6de0178d33af2635d4d
lukaskreibig <lukatze@posteo.de>
2021-07-28 18:17:21 +0200

    add connection release

--- models/leaderboard-models.js
+++ models/leaderboard-models.js
@@ -14,15 +14,13 @@ Leaderboard.findWd = (callback) => {
 };
 
 Leaderboard.findWdDays = (days) => {
-  return (
-    db
-      .query(
-        "SELECT t1.beekeeper_id, t1.avg_wd, t2.avg_wd_before, t1.wd_rank, t2.wd_rank_before, t1.username, t1.email, t1.region, t1.country, t1.experience, t1.beehives, t1.apiaries, t1.profile_picture, t1.bio FROM (SELECT ROUND(AVG(weight_delta.weight_delta),2) AS `avg_wd`, beekeeper.beekeeper_id, beekeeper.username, beekeeper.email, beekeeper.region, beekeeper.country, beekeeper.experience, beekeeper.beehives, beekeeper.apiaries, beekeeper.profile_picture, beekeeper.bio, RANK () OVER ( ORDER BY ROUND(AVG(weight_delta.weight_delta),2) DESC ) wd_rank FROM weight_delta INNER JOIN beekeeper ON beekeeper.beekeeper_id = weight_delta.beekeeper_id WHERE date > now() - INTERVAL ? day GROUP BY beekeeper.beekeeper_id, beekeeper.username, beekeeper.email, beekeeper.region, beekeeper.country, beekeeper.experience, beekeeper.beehives, beekeeper.apiaries, beekeeper.profile_picture, beekeeper.bio) t1 LEFT JOIN (SELECT ROUND(AVG(weight_delta.weight_delta),2) AS `avg_wd_before`, weight_delta.beekeeper_id, RANK () OVER ( ORDER BY ROUND(AVG(weight_delta.weight_delta),2) DESC ) wd_rank_before FROM weight_delta WHERE (date BETWEEN now() - INTERVAL 2 * ? DAY AND now() - INTERVAL ? DAY) GROUP BY weight_delta.beekeeper_id) t2 ON (t1.beekeeper_id = t2.beekeeper_id) ORDER BY t1.avg_wd DESC",
-        [days, days, days]
-      )
-      // .connection.release()
-      .then(([results]) => results)
-  );
+  return db
+    .query(
+      "SELECT t1.beekeeper_id, t1.avg_wd, t2.avg_wd_before, t1.wd_rank, t2.wd_rank_before, t1.username, t1.email, t1.region, t1.country, t1.experience, t1.beehives, t1.apiaries, t1.profile_picture, t1.bio FROM (SELECT ROUND(AVG(weight_delta.weight_delta),2) AS `avg_wd`, beekeeper.beekeeper_id, beekeeper.username, beekeeper.email, beekeeper.region, beekeeper.country, beekeeper.experience, beekeeper.beehives, beekeeper.apiaries, beekeeper.profile_picture, beekeeper.bio, RANK () OVER ( ORDER BY ROUND(AVG(weight_delta.weight_delta),2) DESC ) wd_rank FROM weight_delta INNER JOIN beekeeper ON beekeeper.beekeeper_id = weight_delta.beekeeper_id WHERE date > now() - INTERVAL ? day GROUP BY beekeeper.beekeeper_id, beekeeper.username, beekeeper.email, beekeeper.region, beekeeper.country, beekeeper.experience, beekeeper.beehives, beekeeper.apiaries, beekeeper.profile_picture, beekeeper.bio) t1 LEFT JOIN (SELECT ROUND(AVG(weight_delta.weight_delta),2) AS `avg_wd_before`, weight_delta.beekeeper_id, RANK () OVER ( ORDER BY ROUND(AVG(weight_delta.weight_delta),2) DESC ) wd_rank_before FROM weight_delta WHERE (date BETWEEN now() - INTERVAL 2 * ? DAY AND now() - INTERVAL ? DAY) GROUP BY weight_delta.beekeeper_id) t2 ON (t1.beekeeper_id = t2.beekeeper_id) ORDER BY t1.avg_wd DESC",
+      [days, days, days]
+    )
+    .connection.release()
+    .then(([results]) => results);
 };
 
 module.exports = Leaderboard;
--- models/profile-models.js
+++ models/profile-models.js
@@ -4,24 +4,24 @@ const db = connection.promise();
 const Edit = {};
 
 Edit.findOne = (user) => {
-	return db
-		.query("SELECT * FROM beekeeper WHERE beekeeper_id=?", [user])
-		.then(([results]) => results);
+  return db
+    .query("SELECT * FROM beekeeper WHERE beekeeper_id=?", [user])
+    .then(([results]) => results);
 };
 
 Edit.update = (id, email) => {
-	return db
-		.query("UPDATE beekeeper SET ? WHERE beekeeper_id=?", [email, id])
-		.then(([results]) => results);
+  return db
+    .query("UPDATE beekeeper SET ? WHERE beekeeper_id=?", [email, id])
+    .then(([results]) => results);
 };
 
 Edit.updatePass = (id, newPass) => {
-	return db
-		.query("UPDATE beekeeper SET password=? WHERE beekeeper_id=?", [
-			newPass,
-			id,
-		])
-		.then(([results]) => results);
+  return db
+    .query("UPDATE beekeeper SET password=? WHERE beekeeper_id=?", [
+      newPass,
+      id,
+    ])
+    .then(([results]) => results);
 };
 
 module.exports = Edit;
